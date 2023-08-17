f04895ff518046e1f0a73e96090dad52ea973308
Giuseppe Dezute Fechio <zzzep@hotmail.com>
2023-08-17 13:46:53 -0300

    add correct types to migration

--- src/data/entities/accounts.go
+++ src/data/entities/accounts.go
@@ -1,6 +1,11 @@
 package entities
 
+import "time"
+
 type Account struct {
-	AccountId      int    `json:"account_id" gorm:"primaryKey"`
-	DocumentNumber string `json:"document_number"`
+	AccountId      int       `gorm:"type:int; primaryKey" json:"account_id"`
+	DocumentNumber string    `gorm:"type:varchar(20)" json:"document_number"`
+	CreatedAt      time.Time `gorm:"type:time; autoCreateTime; default:CURRENT_TIMESTAMP(3)"`
+	UpdatedAt      time.Time `gorm:"type:time; autoUpdateTime; default:CURRENT_TIMESTAMP(3)"`
+	DeletedAt      time.Time `gorm:"type:time; default:null"`
 }
--- src/data/entities/transactions.go
+++ src/data/entities/transactions.go
@@ -1,9 +1,14 @@
 package entities
 
+import "time"
+
 type Transaction struct {
-	TransactionId   int           `gorm:"primaryKey"`
-	AccountId       int           `json:"account_id"`
-	OperationTypeId OperationType `json:"operation_type_id"`
-	Amount          float64       `json:"amount"`
-	EventDate       string
+	TransactionId   int           `gorm:"type:int; primaryKey"`
+	Account         Account       `gorm:"type:int; references:AccountId" json:"account_id"`
+	OperationTypeId OperationType `gorm:"type:enum('1','2','3','4')" json:"operation_type_id"`
+	Amount          float64       `gorm:"type:float" json:"amount"`
+	EventDate       time.Time     `gorm:"type:time; autoCreateTime; default:CURRENT_TIMESTAMP(3)"`
+	CreatedAt       time.Time     `gorm:"type:time; autoCreateTime; default:CURRENT_TIMESTAMP(3)"`
+	UpdatedAt       time.Time     `gorm:"type:time; autoUpdateTime; default:CURRENT_TIMESTAMP(3)"`
+	DeletedAt       time.Time     `gorm:"type:time; default:null"`
 }
